0764b3de732442f1921b91f7317b4e4131961ddf
joaopedrofump <joaofurriel@wifi-interno.fe.up.pt>
2016-11-28 11:55:20 +0000

    correcting codacy errors

--- build/testPDFMakeJS.js
+++ build/testPDFMakeJS.js
@@ -1,16 +1,14 @@
-function onLoad() {
-	var buttonToPrint = document.getElementById('b1');
-	buttonToPrint.addEventListener("click", printFile, false);
-}
-
-
 function printFile() {
-  var docDefinition = {content: "Test PDF Make in PDF File" };
+  var docDefinition = {content: 'Test PDF Make in PDF File' };
   //var docDefintion2 = {content: [{text: "test", fontSize: 20}]};
-  var docDefinition3 = {content: "Test PDF Make in PDF File - 3" };
+  var docDefinition3 = {content: 'Test PDF Make in PDF File - 3' };
   pdfMake.createPdf(docDefinition).download();
   pdfMake.createPdf(docDefinition3).download();
-  console.log("testButton");
 }
 
-window.addEventListener("load", onLoad, false);
+function onLoad() {
+	var buttonToPrint = document.getElementById("b1");
+	buttonToPrint.addEventListener('click', printFile, false);
+}
+
+window.addEventListener('load', onLoad, false);
